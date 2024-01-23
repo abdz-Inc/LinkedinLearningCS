7b860d14e91b491353d858222eee2ea12aa5494c
abdulra <abdulra@ABDULRA-IN-LE01>
2024-01-23 15:18:53 +0530

    Add project files.

---
+++ LinkedinLearning/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedinLearning;
+
+namespace Linked
+{
+    public class Progam {
+        public delegate float shippingDelegate(int cost);
+        public static void Main(string[] args) {
+            
+
+            shippingDelegate shipping;
+            Console.WriteLine("Enter the zone : ");
+
+            int zone = int.Parse(Console.ReadLine());
+
+            switch(zone)
+            {
+                case 1:
+                    shipping = new ShippingZones(25).shippingcost;
+                    break;
+                case 2:
+                    shipping = new ShippingZones(12,true, 25).shippingcost;
+                    break;
+                case 3:
+                    shipping = new ShippingZones(8).shippingcost;
+                    break;
+                case 4:
+                    shipping = new ShippingZones(4, true, 25).shippingcost;
+                    break;
+
+                default:
+                    shipping = new ShippingZones(zone).shippingcost;
+                    break;
+
+
+            }
+
+            Console.WriteLine("Enter the Cost : ");
+
+            int cost = int.Parse(Console.ReadLine());
+            Console.WriteLine("The total shipping charges are : " + shipping(cost));
+
+        }
+}
+
+}
---
+++ LinkedinLearning/ShippingDelegates.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinLearning
+{
+    public class ShippingZones
+    {
+        private int _zonepercent;
+        private int _risk;
+        private bool _highrisk;
+
+        public int Zonepercent{
+            get; set;
+            }
+
+        public int Risk
+        {
+            get => _risk;
+            set=> _risk=value;
+            
+        }
+
+        public bool Highrisk
+        {
+            get;
+            set;
+        }
+
+        public ShippingZones(int zone, bool highrisk = false, int risk = 0)
+        {
+            Zonepercent = zone;
+            Risk = risk;
+            Highrisk = highrisk;
+        }
+
+        public float shippingcost(int cost)
+        {
+            return Highrisk?Risk:0 + ((cost * Zonepercent) / 100);
+        }
+    }
+}
