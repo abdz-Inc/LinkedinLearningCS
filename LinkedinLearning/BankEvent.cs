7330f06d790197c49733547e9f43d6042af48e84
abdulra <abdulra@ABDULRA-IN-LE01>
2024-01-24 14:50:57 +0530

    Events

---
+++ LinkedinLearning/BankEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedinLearning
+{
+
+    public class MyEventArgs : EventArgs
+    {
+        public int balance;
+    }
+
+    internal class BankEvent
+    {
+        private int _balance;
+        private int _limit;
+        public event EventHandler<MyEventArgs> BalanceChanged = delegate {  };
+
+
+        public int Balance { 
+            get { return _balance; }
+            set {
+                this._balance = value;
+                BalanceChanged(this, new MyEventArgs()
+                {
+                    balance = _balance
+                });
+            }
+        }
+
+        public int Limit
+        {
+            get; set;
+        }
+
+        public BankEvent(int balance=0, int limit = 500)
+        {
+            Balance = balance;
+            Limit = limit;
+        }
+
+    }
+}
--- LinkedinLearning/Program.cs
+++ LinkedinLearning/Program.cs
@@ -5,40 +5,79 @@ namespace Linked
 {
     public class Progam {
         public delegate float shippingDelegate(int cost);
-        public static void Main(string[] args) {
-            
 
-            shippingDelegate shipping;
-            Console.WriteLine("Enter the zone : ");
+        public static void Listenbalance(object sender, MyEventArgs e)
+        {
+            Console.WriteLine("Your balance is : "+e.balance);
+        }
 
-            int zone = int.Parse(Console.ReadLine());
-
-            switch(zone)
+        public static void Listenbalancelimit(object sender, MyEventArgs e)
+        {
+            if (e.balance >= ((BankEvent)sender).Limit)
             {
-                case 1:
-                    shipping = new ShippingZones(25).shippingcost;
-                    break;
-                case 2:
-                    shipping = new ShippingZones(12,true, 25).shippingcost;
-                    break;
-                case 3:
-                    shipping = new ShippingZones(8).shippingcost;
-                    break;
-                case 4:
-                    shipping = new ShippingZones(4, true, 25).shippingcost;
-                    break;
+                Console.WriteLine("You have reached your target " + e.balance);
+            }
+        }
 
-                default:
-                    shipping = new ShippingZones(zone).shippingcost;
-                    break;
+        public static void Main(string[] args) {
 
+            //shippingDelegates
+
+            //bool quit = false;
+            //do
+            //{
+            //    shippingDelegate shipping;
+            //    Console.WriteLine("Enter the zone : ");
+
+            //    int zone = int.Parse(Console.ReadLine());
+
+            //    switch (zone)
+            //    {
+            //        case 1:
+            //            shipping = new ShippingZones(25).shippingcost;
+            //            break;
+            //        case 2:
+            //            shipping = new ShippingZones(12, true, 25).shippingcost;
+            //            break;
+            //        case 3:
+            //            shipping = new ShippingZones(8).shippingcost;
+            //            break;
+            //        case 4:
+            //            shipping = new ShippingZones(4, true, 25).shippingcost;
+            //            break;
+
+            //        default:
+            //            shipping = null;
+            //            quit = true;
+            //            break;
+
+
+            //    }
+
+            //    Console.WriteLine("Enter the Cost : ");
+
+            //    int cost = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("The total shipping charges are : " + shipping(cost));
+            //} while (!quit);
+
+            //BankEvent
+
+            BankEvent bank = new BankEvent();
+            bank.BalanceChanged += delegate (object sender, MyEventArgs e) { Console.WriteLine("Your balance is : " + e.balance); };
+            bank.BalanceChanged += new EventHandler<MyEventArgs>(Listenbalancelimit);
+            int deposit;
+            while (true)
+            {
+                Console.WriteLine(" Enter amount to deposit : ");
+                string response = Console.ReadLine();
+                if (response == "exit")
+                {
+                    break;
+                }
+                deposit = int.Parse(response);
+                bank.Balance += deposit;
 
             }
-
-            Console.WriteLine("Enter the Cost : ");
-
-            int cost = int.Parse(Console.ReadLine());
-            Console.WriteLine("The total shipping charges are : " + shipping(cost));
 
         }
 }
